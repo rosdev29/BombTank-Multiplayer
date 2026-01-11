301f45bd1c867064de6b3295ad4bca889aa42f10
An <garanhutmau256@gmail.com>
2026-01-12 02:42:08 +0700

    cai thien tan cong

--- Assets/Scripts/Core/Player/BoPhongDan.cs
+++ Assets/Scripts/Core/Player/BoPhongDan.cs
@@ -11,10 +11,18 @@ public class BoPhongDan : NetworkBehaviour
     [SerializeField] private Transform DiemSpawnDan;
     [SerializeField] private GameObject ServerDanPrefab;
     [SerializeField] private GameObject ClientDanPrefab;
+    [SerializeField] private GameObject hieuUngLoeNong;
+    [SerializeField] private Collider2D vaChamNguoiChoi;
 
     [Header("Settings")]
     [SerializeField] private float TocDoDan;
+    [SerializeField] private float tanSuatTanCong;
+    [SerializeField] private float thoiGianHieuUngBan;
+
     private bool duocTanCong;
+    private float thoiDiemTanCongTruoc;
+    private float henGioLoeNong;
+
 
     public override void OnNetworkSpawn()
     {
@@ -30,13 +38,27 @@ public class BoPhongDan : NetworkBehaviour
 
     private void Update()
     {
+        if(henGioLoeNong > 0f)
+        {
+            henGioLoeNong -= Time.deltaTime;
+            if(henGioLoeNong <= 0f)
+            {
+                hieuUngLoeNong.SetActive(false);
+            }
+        }
         if (!IsOwner) { return; }
+
         if (!duocTanCong) { return; }
+
+
+        if (Time.time < (1 / tanSuatTanCong) + thoiDiemTanCongTruoc) { return; }
+        
 
         xuLyBanChinhServerRpc(DiemSpawnDan.position, DiemSpawnDan.up);
 
         spawnDanGia(DiemSpawnDan.position, DiemSpawnDan.up);
 
+        thoiDiemTanCongTruoc = Time.time;
     }
 
     private void xuLyTanCongChinh(bool duocTanCong)
@@ -55,7 +77,16 @@ public class BoPhongDan : NetworkBehaviour
 
         danInstance.transform.up = huongDi;
 
+        Physics2D.IgnoreCollision(vaChamNguoiChoi, danInstance.GetComponent<Collider2D>());
+
+        if (danInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.velocity = rb.transform.up * TocDoDan;
+        }
+
+        spawnDanGiaClientRpc(viTriSpawn, huongDi);
     }
+
     [ClientRpc]
 
     private void spawnDanGiaClientRpc(Vector3 viTriSpawn, Vector3 huongDi)
@@ -68,6 +99,9 @@ public class BoPhongDan : NetworkBehaviour
 
     private void spawnDanGia(Vector3 viTriSpawn, Vector3 huongDi)
     {
+        hieuUngLoeNong.SetActive(true);
+        henGioLoeNong = thoiGianHieuUngBan;
+
         GameObject danInstance = Instantiate(
             ClientDanPrefab,
             viTriSpawn,
@@ -75,5 +109,11 @@ public class BoPhongDan : NetworkBehaviour
 
         danInstance.transform.up = huongDi;
 
+        Physics2D.IgnoreCollision(vaChamNguoiChoi, danInstance.GetComponent<Collider2D>());
+
+        if(danInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb ))
+        {
+            rb.velocity = rb.transform.up * TocDoDan;
+        }
     }
 }
