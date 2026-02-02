c1861a037944c978e72b5fbc1ae2b26dd529f217
An <garanhutmau256@gmail.com>
2026-02-03 02:25:15 +0700

    hien thi mau

--- Assets/Scripts/Core/Combat/mau.cs
+++ Assets/Scripts/Core/Combat/mau.cs
@@ -1,18 +1,47 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class mau : MonoBehaviour
+public class Mau : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [field: SerializeField] public int MauToiDa {  get; private set; } = 100;
+
+    public NetworkVariable<int> MauHienTai = new NetworkVariable<int>();
+
+    private bool daChet;
+
+    public Action<Mau> KhiChet;
+
+    public override void OnNetworkSpawn()
     {
-        
+        if (!IsServer) { return; }
+
+        MauHienTai.Value = MauToiDa;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void NhanSatThuong(int giaTriSatThuong)
     {
-        
+        ThayDoiMau(-giaTriSatThuong);
+    }
+
+    public void HoiMau(int giaTriHoi)
+    {
+        ThayDoiMau(giaTriHoi);
+    }
+
+    private void ThayDoiMau(int value)
+    {
+        if (daChet) { return;  }
+
+        int MauMoi = MauHienTai.Value + value;
+        MauHienTai.Value = Mathf.Clamp(MauMoi, 0, MauToiDa);
+
+        if(MauHienTai.Value == 0)
+        {
+            KhiChet?.Invoke(this);
+            daChet = true;
+        }
     }
 }
