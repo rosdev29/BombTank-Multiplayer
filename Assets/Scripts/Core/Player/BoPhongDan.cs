012df1cf781408b0bbb9fdce9fef18ddb42d5f47
An <garanhutmau256@gmail.com>
2026-02-02 22:26:55 +0700

    update

--- Assets/Scripts/Core/Player/BoPhongDan.cs
+++ Assets/Scripts/Core/Player/BoPhongDan.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -93,7 +93,7 @@ public class BoPhongDan : NetworkBehaviour
     {
         if (!IsOwner) { return; }
         spawnDanGia(viTriSpawn, huongDi);
-        spawnDanGiaClientRpc(viTriSpawn, huongDi);
+        // spawnDanGiaClientRpc(viTriSpawn, huongDi); // đây là recursion vô hạn
 
     }
 
