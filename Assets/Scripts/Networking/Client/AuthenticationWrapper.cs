2a5e94a7e4ff51583aba241d6735176e821e4de9
An <duylt0022@ut.edu.vn>
2026-03-06 23:55:24 +0700

    implement with retry logic

--- Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
+++ Assets/Scripts/Networking/Client/AuthenticationWrapper.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Authentication;
 using UnityEngine;
 
-public class AuthenticationWrapper : MonoBehaviour
+public static class AuthenticationWrapper
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static AuthState AuthState { get; private set; } = AuthState.NotAuthenticated;
 
-    // Update is called once per frame
-    void Update()
+    public static async Task<AuthState> DoAuth(int maxTries = 5)
     {
-        
+        if (AuthState == AuthState.NotAuthenticated)
+        {
+            return AuthState;
+        }
+
+        int tries = 0;
+        while (AuthState == AuthState.Authenticating && tries < maxTries)
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            
+            if(AuthenticationService.Instance.IsSignedIn && AuthenticationService.Instance.IsAuthorized)
+            {
+                AuthState = AuthState.Authenticated;
+                break;
+            }    
+
+            tries++;
+            await Task.Delay(1000);
+        }
+
+        return AuthState;
     }
+}
+
+public enum AuthState
+{
+    NotAuthenticated,
+    Authenticating,
+    Authenticated,
+    Error,
+    Timeout,
 }
