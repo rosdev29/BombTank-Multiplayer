0813cd06e190bc8766e2a7e06260c8acfcc7a491
Duy <duylt0022@ut.edu.vn>
2026-03-07 00:04:17 +0700

    improve ClientGameManager initialization flow

--- Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -1,12 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Services.Core;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ClientGameManager 
 {
-   public async Task InitAsync()
+    private const string MenuSceneName = "Menu";
+   public async Task<bool> InitAsync()
     {
+        await UnityServices.InitializeAsync();
 
+        AuthState authState = await AuthenticationWrapper.DoAuth();
+
+        if(authState == AuthState.Authenticated)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void GoToMenu()
+    {
+        SceneManager.LoadScene("MenuSceneName");
     }
 }
