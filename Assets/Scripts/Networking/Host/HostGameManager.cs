0d2f523bd055c7fa2bf4b8104917dac4ceb1e39f
An <duylt0022@ut.edu.vn>
2026-03-10 23:51:46 +0700

    testing allocation

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -1,9 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
+using Unity.Networking.Transport.Relay;
 
-public class HostGameManager 
+public class HostGameManager
 {
-   
+    private Allocation allocation;
+    private RelayServerData relayServerData;
+    private bool hasRelayServerData;
+    private const int MaxConnections = 20;
+
+    public RelayServerData RelayServerData => relayServerData;
+    public bool HasRelayServerData => hasRelayServerData;
+
+    public async Task StartHostAsync()
+    {
+        try
+        {
+            allocation = await RelayService.Instance.CreateAllocationAsync(MaxConnections);
+            relayServerData = allocation.ToRelayServerData("udp");
+            hasRelayServerData = true;
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
 }
