123db22f4c8423733ee09b8eee5f799052f1c6ac
Duy <duylt0022@ut.edu.vn>
2026-03-11 00:20:14 +0700

    allocating a relay

--- Assets/Scripts/Networking/Host/HostGameManager.cs
+++ Assets/Scripts/Networking/Host/HostGameManager.cs
@@ -4,13 +4,17 @@ using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
 using Unity.Networking.Transport.Relay;
-
+using Unity.Netcode.Transports.UTP;
+using Unity.Netcode;
+using UnityEngine.SceneManagement;
 public class HostGameManager
 {
     private Allocation allocation;
+    private string joinCode;
     private RelayServerData relayServerData;
     private bool hasRelayServerData;
     private const int MaxConnections = 20;
+    private const string GameScenceName = "Game";
 
     public RelayServerData RelayServerData => relayServerData;
     public bool HasRelayServerData => hasRelayServerData;
@@ -26,6 +30,24 @@ public class HostGameManager
         catch (Exception e)
         {
             Debug.Log(e);
+            return;
         }
+        try
+        {
+            joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+            Debug.Log(joinCode);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        if (transport != null && hasRelayServerData)
+            transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(GameScenceName, LoadSceneMode.Single);
     }
 }
--- Assets/Scripts/Networking/Host/HostSingleton.cs
+++ Assets/Scripts/Networking/Host/HostSingleton.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class HostSingleton : MonoBehaviour
