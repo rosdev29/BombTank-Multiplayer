226047022004cb1f9787437190f4da0839892589
An <duylt0022@ut.edu.vn>
2026-03-11 00:55:22 +0700

    joining a relay

--- Assets/Scripts/Networking/Client/ClientGameManager.cs
+++ Assets/Scripts/Networking/Client/ClientGameManager.cs
@@ -1,20 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using Unity.Services.Core;
+using Unity.Services.Relay;
+using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Networking.Transport.Relay;
 
-public class ClientGameManager 
+public class ClientGameManager
 {
-   private const string MenuSceneName = "Menu";
-   public async Task<bool> InitAsync()
+    private JoinAllocation allocation;
+
+    private const string MenuSceneName = "Menu";
+    public async Task<bool> InitAsync()
     {
         await UnityServices.InitializeAsync();
 
         AuthState authState = await AuthenticationWrapper.DoAuth();
 
-        if(authState == AuthState.Authenticated)
+        if (authState == AuthState.Authenticated)
         {
             return true;
         }
@@ -24,5 +32,25 @@ public class ClientGameManager
     public void GoToMenu()
     {
         SceneManager.LoadScene(MenuSceneName);
+    }
+
+    public async Task StartClientAsync(string joinCode)
+    {
+        try
+        {
+            allocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+            return;
+        }
+
+        RelayServerData relayServerData = allocation.ToRelayServerData("udp");
+        UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+        if (transport != null)
+            transport.SetRelayServerData(relayServerData);
+
+        NetworkManager.Singleton.StartClient();
     }
 }
