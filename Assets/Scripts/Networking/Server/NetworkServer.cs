dcb1793b7e5baef41f918ad571345112359e7eea
Duy <duylt0022@ut.edu.vn>
2026-03-22 23:09:16 +0700

    server build prep

--- Assets/Scripts/Networking/Server/NetworkServer.cs
+++ Assets/Scripts/Networking/Server/NetworkServer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class NetworkServer : IDisposable
@@ -20,6 +21,13 @@ public class NetworkServer : IDisposable
         previousApprovalCallback = networkManager.ConnectionApprovalCallback;
         networkManager.ConnectionApprovalCallback = ApprovalCheck;
         networkManager.OnServerStarted += OnNetworkReady;
+    }
+
+    public bool OpenConnection(string ip, int port)
+    {
+        UnityTransport transport = networkManager.gameObject.GetComponent<UnityTransport>();
+        transport.SetConnectionData(ip, (ushort)port);
+        return networkManager.StartServer();
     }
 
     private void ApprovalCheck(
