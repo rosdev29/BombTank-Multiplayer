17843a2fcee2dfcb05726966486fb55a6dbd6657
PhucIT <pvo68586@gmail.com>
2026-05-28 21:08:53 +0700

    thêm AudioManager và SFX cho tank và pickcoin

--- Assets/Scripts/Core/Coins/BountyCoin.cs
+++ Assets/Scripts/Core/Coins/BountyCoin.cs
@@ -6,21 +6,18 @@ public class BountyCoin : Coin
 {
     public override int Collect()
     {
-        Debug.Log("COIN COLLECT FUNCTION RUNNING");
-
         if (!IsServer)
         {
             Show(false);
             return 0;
         }
 
-        if (alreadyCollected) { return 0; }
+        if (alreadyCollected)
+        {
+            return 0;
+        }
 
         alreadyCollected = true;
-
-        Debug.Log("PLAY COIN SOUND");
-
-        AudioManager.Instance.PlaySFX(AudioManager.Instance.coinPickup);
 
         Destroy(gameObject);
 
